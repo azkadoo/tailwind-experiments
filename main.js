2c4f451989498ef37eaf701fcbde64f97f26ac4f
Azka <muhammadazkanurazhim@gmail.com>
2026-04-16 14:07:49 +0700

    fix: responsive.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js";
 
 const typed = new Typed("#textRotator", {
-  strings: ["UI/UX Designer", "Web Developer (Soon)", "Gamify Education"],
+  strings: ["UI/UX Designer", "Web Developer", "Gamification Enthusiast"],
   typeSpeed: 50,
   loop: true,
 });
